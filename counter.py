9032c015f0fceb2d8085d9482a08da4e2fd4103e
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2023-08-18 14:58:13 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -2,11 +2,11 @@
 import flet
 from flet import Page, TextField, Row, IconButton, icons
 
-
 def main(page: Page):
     page.title = "Counter App"
     page.vertical_alignment = "center"
 
+    
     txt_field = TextField(value="0", width=100, text_align="right")
     def minus_clicked(e):
         txt_field.value = int(txt_field.value) - 1
