e8a96b4c7027ba7e924c7931f1066186892d7498
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2023-08-22 14:55:18 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 from calendar import c
 import flet
+
 from flet import (
     Page,
     TextField,
@@ -16,6 +17,7 @@ from flet import (
     colors,
     icons,
 )
+
 class Task(UserControl):
     def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
